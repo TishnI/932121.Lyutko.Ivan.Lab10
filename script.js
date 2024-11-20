ecc171a7345a32d6aba6b6cf13af9121b6ce267b
Ivan Lyutko <ivan.lyutko@bk.ru>
2024-11-20 21:45:35 +0700

    Да будет свет!

--- script.js
+++ script.js
@@ -3,3 +3,30 @@
 document.querySelector('.js-curtain').addEventListener('click', function(){
     this.style.bottom = "100vh";
 })
+
+var lampSwitchElement = document.querySelector('.js-lamp-switch');
+var lampIsOn = false;
+
+lampSwitchElement.addEventListener('mousedown', function(){
+    this.style.top = "130px";
+})
+
+var lightElement = document.querySelector('.js-lamp-light');
+lampSwitchElement.addEventListener('mouseup', function(){
+    this.style.top = '110px';
+    lampIsOn = !lampIsOn;
+    console.log(lampIsOn);
+
+    if(lampIsOn)
+    {
+        lightElement.style.opacity = '0.5';
+    }
+    else
+    {
+        lightElement.style.opacity = '0';
+    }
+})
+
+
+
+
